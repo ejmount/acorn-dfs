bc581da3c7e5db6683d581b55969960ee573858f
Ewan Mount <ejmount@users.noreply.github.com>
2026-04-18 11:38:50 +0100

    refactor: Removing dead FaultValue::unpack function

--- lib/src/new_map/mod.rs
+++ lib/src/new_map/mod.rs
@@ -59,11 +59,7 @@ pub enum IoError {
 
 #[derive(Debug, Clone)]
 pub(crate) struct FaultValue<T>(pub(crate) T, pub(crate) Vec<Fault>);
-impl<T> FaultValue<T> {
-    pub(crate) fn unpack(FaultValue(t, f): Self) -> (T, Vec<Fault>) {
-        (t, f)
-    }
-}
+
 impl<T> From<T> for FaultValue<T> {
     fn from(value: T) -> Self {
         FaultValue(value, vec![])
